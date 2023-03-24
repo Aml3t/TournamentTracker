e498abff313b07b1b9a6880db90d2be079aae4a2
Aml3t <fischerparsos@gmail.com>
2023-03-24 11:21:10 +0200

    files

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,23 @@
+﻿namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one team in the matchup
+    /// </summary>
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// The score of the particular team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// The matchup that this team came
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,23 @@
+﻿namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one match in the tournament
+    /// </summary>
+    public class MatchupModel
+    {
+        /// <summary>
+        /// The list of the competiting teams in this match.
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        
+        /// <summary>
+        /// The winner of the match.
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Which round this match is a part of.
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
