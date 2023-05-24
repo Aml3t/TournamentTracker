c12c897b7c2ae1172052781676c2a40ff4626e53
Aml3t <fischerparsos@gmail.com>
2023-05-24 13:48:15 +0300

    Setting the SMS function

--- TrackerLibrary/SMSLogic.cs
+++ TrackerLibrary/SMSLogic.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace TrackerLibrary
 {
     public class SMSLogic
     {
-        public static void SendSMSMessage(string to, string message)
+        public static void SendSMSMessage(string to, string textMessage)
         {
-            string accountSid = "";
-            string authToken = "";
+            string accountSid = GlobalConfig.AppKeyLookup("smsAccountSid");
+            string authToken = GlobalConfig.AppKeyLookup("smsAuthToken");
+            string fromPhoneNumber = GlobalConfig.AppKeyLookup("smsFromPhoneNumber");
+
             TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber(to),
+                from: new PhoneNumber(fromPhoneNumber),
+                body: textMessage
+                );
         }
     }
 }
